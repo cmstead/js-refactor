78b475679847ea74fe9b47355b359fec428418bc
Chris Stead <cmstead@gmail.com>
2016-02-19 23:59:00 -0800

    Refactored edit-factory module.

--- modules/shared/edit-factory.js
+++ modules/shared/edit-factory.js
@@ -3,47 +3,74 @@
 var vscode = require('vscode');
 var j = require('jfp');
 
-function buildTextEdit (coords, text) {
-    var start = new vscode.Position(coords.start[0], coords.start[1]),
-        end = new vscode.Position(coords.end[0], coords.end[1]),
-        range = new vscode.Range(start, end);
-
-    return new vscode.TextEdit(range, text);
+function positionFactory (line, column){
+    return new vscode.Position(line, column);
 }
 
-function buildWorkspaceEdit () {
+// This will be important as coordinate object is converted to new format
+function positionFactoryFacade (location) {
+    return positionFactory(location[0], location[1]);
+}
+
+function rangeFactory (start, end){
+    var startPosition = positionFactoryFacade(start);
+    var endPosition = positionFactoryFacade(end);
+    
+    return new vscode.Range(startPosition, endPosition);
+}
+
+function textEditFactory (coords, content){
+    var range = rangeFactory(coords.start, coords.end);
+    return new vscode.TextEdit(range, content);
+}
+
+function workspaceEditFactory () {
     return new vscode.WorkspaceEdit();
 }
 
-// Builds a "set" type edit object to update the view
-function buildSetEdit (uri, coords, text) {
-    var edit = buildWorkspaceEdit();
-    edit.set(uri, [buildTextEdit(coords, text)]);
+function setEditFactory (uri, edits){
+    var edit = workspaceEditFactory();
+    
+    edit.set(uri, edits);
     
     return edit;
+}
+
+function replaceEditFactory (uri, edits){
+    var edit = workspaceEditFactory();
+    
+    edit.replace(uri, edits);
+    
+    return edit;
+}
+
+// Composite construction functions
+
+// Builds a "set" type edit object to update the view
+function buildSetEdit (uri, coords, text) {
+    var textEdit = textEditFactory(coords, text);
+    return setEditFactory(uri, [textEdit]);
 }
 
 // Builds a "replace" type edit object to update the view
 function buildReplaceEdit (uri, coords, text) {
-    var edit = buildWorkspaceEdit();
-    edit.replace(uri, [buildTextEdit(coords, text)]);
-
-    return edit
+    var textEdit = textEditFactory(coords, text);
+    return replaceEditFactory(uri, [textEdit]);
 }
 
 function buildMultipleSetEdits (uri, edits){
-    var textEdits = edits.reduce(function (list, edit) { return j.conj(buildTextEdit(edit.coords, edit.value), list); }, []);
-    var edit = buildWorkspaceEdit();
-    
-    edit.set(uri, textEdits);
-    
-    return edit;
+    var textEdits = edits.map(function (edit) { return textEditFactory(edit.coords, edit.value); });
+    return setEditFactory(uri, textEdits);
+}
+
+function buildMultipleReplaceEdits (uri, edits){
+    var textEdits = edits.map(function (edit) { return textEditFactory(edit.coords, edit.value); });
+    return replaceEditFactory(uri, textEdits);
 }
 
 module.exports = {
     buildMultipleSetEdits: buildMultipleSetEdits,
-    buildTextEdit: buildTextEdit,
-    buildWorkspaceEdit: buildWorkspaceEdit,
     buildSetEdit: buildSetEdit,
-    buildReplaceEdit: buildReplaceEdit
+    buildMultipleReplaceEdits: buildMultipleSetEdits,
+    buildReplaceEdit: buildSetEdit
 };
