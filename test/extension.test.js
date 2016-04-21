59367c76e3c6daef2f61004766401e865bc82cac
Chris Stead <Chris.Stead@hunterindustries.com>
2016-04-21 09:08:47 -0700

    Added checks for configuration values

--- test/extension.test.js
+++ test/extension.test.js
@@ -5,14 +5,22 @@ var mockery = require('mockery');
 var vscodeFake = require('./test-utils/vscode-fake');
 var vscodeFactory = require('../modules/shared/vscodeFactory');
 var testUtils = require('./test-utils/test-utils');
+var packageObj = require('../package.json');
 
 require('./test-utils/approvals')();
+
+function addOnCommandToSet (commandSet, commandStr){
+    var key = commandStr.replace('onCommand:', '');
+    commandSet[key] = true;
+    return commandSet;
+}
 
 describe('register actions', function() {
 
     var extension;
     var commandRegister;
     var commandKeys;
+    var onCommandSet;
 
     beforeEach(function() {
         mockery.enable({
@@ -24,6 +32,9 @@ describe('register actions', function() {
         extension = require('../extension');
 
         extension.activate({ subscriptions: [] });
+
+        commandKeys = Object.keys(vscodeFake.commands.getRegisteredCommandList());
+        onCommandSet = packageObj.activationEvents.reduce(addOnCommandToSet, {});
     });
 
     afterEach(function() {
@@ -31,11 +42,32 @@ describe('register actions', function() {
         mockery.disable();
     });
 
-    it('should have all known actions registered', function() {
+    // Needs line ending normalization before this will always work
+    it.skip('should have all known actions registered', function() {
         var registeredCommands = vscodeFake.commands.getRegisteredCommandList();
         var approvalResult = testUtils.objectFunctionsToSource(registeredCommands);
         
         this.verify(testUtils.prettyJson(approvalResult));
     });
+    
+    it('should match every command with an onCommand value in the package.json file', function () {
+        commandKeys.forEach(function (key) {
+            assert.equal(onCommandSet[key], true);
+        });
+    });
 
+    it('should have a one-to-one relationship between actions and activationEvents values', function () {
+        assert.equal(packageObj.activationEvents.length, commandKeys.length);
+    });
+    
+    it('should match every command with a command registration in the package.json file', function () {
+        packageObj.contributes.commands.forEach(function (record) {
+            assert.equal(typeof vscodeFake.commands.getRegisteredCommand(record.command), 'function');
+        });
+    });
+
+    it('should have a one-to-one relationship between actions and commands values', function () {
+        assert.equal(packageObj.contributes.commands.length, commandKeys.length);
+    });
+    
 });
