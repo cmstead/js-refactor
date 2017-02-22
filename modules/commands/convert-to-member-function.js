80a95e6451af87f1a27f059e6a2c53a3013547e9
Chris Stead <Chris.Stead@hunterindustries.com>
2017-02-22 12:57:47 -0800

    Began refactoring to close over vsEditor object

--- modules/commands/convert-to-member-function.js
+++ modules/commands/convert-to-member-function.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var editActions = require('../shared/edit-actions');
+var editActionsFactory = require('../shared/edit-actions-factory');
 var logger = require('../shared/logger-factory')();
 var refactoring = require('../refactoring-logic/convert-to-member-function');
 var selectionFactory = require('../shared/selection-factory');
@@ -8,30 +8,31 @@ var utilities = require('../shared/utilities');
 
 module.exports = function (vsEditor, callback) {
     var selection = selectionFactory(vsEditor).getSelection(0);
+    var editActions = editActionsFactory(vsEditor);
 
     function applyRefactor() {
         var coords = utilities.buildCoords(vsEditor, 0);
 
         selection[0] = refactoring.refactorToMemberFunction(selection[0]);
-        return editActions.applySetEdit(vsEditor, selection.join('\n'), coords);
+        return editActions.applySetEdit(selection.join('\n'), coords);
     }
 
 
     function getErrorMessage() {
         var message = '';
-        
+
         if (selection === null) {
             message = 'Cannot perform member function conversion on an empty selection.';
         } else if (!refactoring.canConvertToMember(selection[0])) {
             message = 'No appropriate named function to convert did you select a line containing a function?';
         }
-        
+
         return message;
     }
 
     function convertToMemberFunction() {
         var message = getErrorMessage();
-        
+
         if (message !== '') {
             logger.log(message);
         } else {
--- modules/commands/convert-to-named-function.js
+++ modules/commands/convert-to-named-function.js
@@ -1,23 +1,25 @@
 'use strict';
 
-var editActions = require('../shared/edit-actions');
+var editActionsFactory = require('../shared/edit-actions-factory');
 var logger = require('../shared/logger-factory')();
 var refactoring = require('../refactoring-logic/convert-to-named-function');
 var selectionFactory = require('../shared/selection-factory');
 var templateUtils = require('../shared/template-utils');
 var utilities = require('../shared/utilities');
 
-module.exports = function (vsEditor) {
-    function applyRefactor(vsEditor, selection) {
+module.exports = function (vsEditor, callback) {
+    var editActions = editActionsFactory(vsEditor);
+
+    function applyRefactor(selection) {
         var coords = utilities.buildCoords(vsEditor, 0);
         var baseContext = templateUtils.buildBaseContext(vsEditor, selection);
         var updatedLine = refactoring.refactorToNamedFunction(selection[0]);
 
         selection[0] = templateUtils.fillTemplate([updatedLine], baseContext);
-        editActions.applySetEdit(vsEditor, selection.join('\n'), coords);
+        return editActions.applySetEdit(selection.join('\n'), coords);
     }
 
-    function convertToNamedFunction(vsEditor) {
+    function convertToNamedFunction() {
         var selection = selectionFactory(vsEditor).getSelection(0);
 
         if (selection === null) {
@@ -25,7 +27,7 @@ module.exports = function (vsEditor) {
         } else if (!refactoring.canRefactorToNamed(selection[0])) {
             logger.log('No appropriate anonymous or member function to convert.');
         } else {
-            applyRefactor(vsEditor, selection);
+            applyRefactor(selection).then(callback);
         }
     }
 
