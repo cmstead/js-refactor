e3ea217fe0fd3699140a3e072c0d14c06bb1d98a
Chris Stead <Chris.Stead@hunterindustries.com>
2017-03-21 10:56:03 -0700

    Extracted source bounds functions to sourceUtils

--- modules/commands/extractVariableFactory.js
+++ modules/commands/extractVariableFactory.js
@@ -31,12 +31,10 @@ function extractVariableFactory(
         }
 
         function buildAndApply(selectionData, scopeData, name, lines) {
-            var bounds = getDocumentScopeBounds(scopeData.scopeBounds);
+            var bounds = sourceUtils.getDocumentScopeBounds(scopeData.scopeBounds);
             var selection = selectionData.selection[0];
-            var scopeSource = getScopeLines(lines, bounds).join('\n');
+            var scopeSource = sourceUtils.getScopeLines(lines, bounds).join('\n');
             var replacementSource = scopeSource.replace(selection, name);
-
-            console.log(scopeSource, replacementSource);
 
             var editActions = editActionsFactory(vsEditor);
 
@@ -54,34 +52,6 @@ function extractVariableFactory(
                 selection: selectionFactory(vsEditor).getSelection(0),
                 selectionCoords: utilities.buildCoords(vsEditor, 0)
             };
-        }
-
-        function getDocumentScopeBounds(scopeBounds) {
-            console.log(scopeBounds);
-            var start = scopeBounds.start;
-            var end = scopeBounds.end;
-
-            return {
-                start: [start[0] - 1, start[1] - 1],
-                end: [end[0] - 1, end[1] - 1]
-            };
-        }
-
-        function getScopeLines(lines, bounds) {
-            var scopeLines = null;
-
-            if(bounds.end[0] === lines.length - 1) {
-                scopeLines = lines.slice(bounds.start[0]);
-            } else {
-                scopeLines = lines.slice(bounds.start[0], bounds.end[0]);
-            }
-
-            var lastIndex = scopeLines.length - 1;
-
-            scopeLines[0] = scopeLines[0].substr(bounds.start[1]);
-            scopeLines[lastIndex] = scopeLines[lastIndex].substr(0, bounds.end[1] + 1);
-
-            return scopeLines;
         }
 
         return function extractAction() {
--- modules/shared/sourceUtils.js
+++ modules/shared/sourceUtils.js
@@ -39,8 +39,37 @@ function sourceUtils(
         };
     }
 
+    function getDocumentScopeBounds(scopeBounds) {
+        var start = scopeBounds.start;
+        var end = scopeBounds.end;
+
+        return {
+            start: [start[0] - 1, start[1] - 1],
+            end: [end[0] - 1, end[1] - 1]
+        };
+    }
+
+    function getScopeLines(lines, bounds) {
+        var scopeLines = null;
+
+        if (bounds.end[0] === lines.length - 1) {
+            scopeLines = lines.slice(bounds.start[0]);
+        } else {
+            scopeLines = lines.slice(bounds.start[0], bounds.end[0]);
+        }
+
+        var lastIndex = scopeLines.length - 1;
+
+        scopeLines[0] = scopeLines[0].substr(bounds.start[1]);
+        scopeLines[lastIndex] = scopeLines[lastIndex].substr(0, bounds.end[1] + 1);
+
+        return scopeLines;
+    }
+    
     return {
         buildEsprimaCoords: buildEsprimaCoords,
+        getDocumentScopeBounds: getDocumentScopeBounds,
+        getScopeLines: getScopeLines,
         matchInLine: matchInLine,
         matchInSource: matchInSource,
         scopeDataFactory: scopeDataFactory
