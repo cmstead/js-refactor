eb532d0cd93b3f64a0cbb090999624201b93e28f
Chris Stead <cmstead@gmail.com>
2017-03-23 16:22:19 -0700

    Fixed readSource for Mac and Linux

--- test/test-utils/read-source.js
+++ test/test-utils/read-source.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 function readSource (path){
     var fileSource = fs.readFileSync(path, { encoding: 'utf8' });
-    return fileSource.split('\r\n');
+    return fileSource.split(/\r?\n/);
 }
 
 module.exports = readSource;
