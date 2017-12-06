d88d913d85fd86511f875a5970b081e24e9e29ee
Chris Stead <cmstead@gmail.com>
2017-12-06 12:45:21 -0800

    Started analyzing getUnboundVariables function to capture more positive hits

--- modules/shared/ast-tools/selectionVariableHelper.js
+++ modules/shared/ast-tools/selectionVariableHelper.js
@@ -13,19 +13,8 @@ function selectionVariableHelper(
             (node) =>
                 astHelper.nodeInCoords(coords, node);
 
-    function getUnboundVars(astCoords, ast) {
-        let currentMemberExpression = null;
-        let boundVars = {};
-        let identifiers = {};
-
-        function processVariable(node) {
-            if (isVar(node)) {
-                const key = node.id.name;
-                boundVars[key] = true;
-            }
-        }
-
-        function processIdentifier(node) {
+    function processIdentifiers(identifiers) {
+        return function (node) {
             const isNonNativeIdentifier = typeHelper.isTypeOf('nonNativeIdentifier')(node);
 
             if (isNonNativeIdentifier && isIdentifier(node) && typeof node.name === 'string') {
@@ -33,13 +22,32 @@ function selectionVariableHelper(
             } else if (isNonNativeIdentifier && typeof node.object.name === 'string') {
                 identifiers[node.object.name] = true;
             }
-        }
+        };
+    }
+
+    function processBoundVars (boundVars) {
+        return function (node) {
+            if (isVar(node)) {
+                const key = node.id.name;
+                boundVars[key] = true;
+            }
+        };
+    }
+
+    function getUnboundVars(astCoords, ast) {
+        let currentMemberExpression = null;
+        
+        let boundVars = {};
+        let identifiers = {};
+
+        const processIdentifier = processIdentifiers(identifiers);
+        const processVariable = processBoundVars(boundVars);
 
         astHelper.traverse(ast, {
             enter: astHelper.onMatch(isNodeInSelection(astCoords), function (node) {
                 if (currentMemberExpression === null) {
                     const isMemberExpression = node.type === 'MemberExpression';
-                    
+
                     processVariable(node);
                     processIdentifier(node);
 
